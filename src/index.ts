ebcd7eaa7f892c5b5f0d7b4f56c711076d2f7ae6
Alphabravoab <9051543+alphabravoab@users.noreply.github.com>
2023-05-31 13:26:15 +0200

    removed prettier and ran yarn format

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,14 @@ import consoleTransport from "./consoleTransport";
 import kafkaLogger from "./kafkaTransport";
 import toString from "./toString";
 
-export { consoleTransport, kafkaLogger, registerLogTransport, toString, LogMessage, Logger, Scopes, Level };
+export {
+  consoleTransport,
+  kafkaLogger,
+  registerLogTransport,
+  toString,
+  LogMessage,
+  Logger,
+  Scopes,
+  Level,
+};
 export default logger;
